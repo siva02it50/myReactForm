d076e03083413d96e1ca0e91936dd7fe2a16184d
siva02it50 <73377841+siva02it50@users.noreply.github.com>
2020-10-24 15:27:55 +0530

    Update useForm.js

--- src/useForm.js
+++ src/useForm.js
@@ -2,10 +2,11 @@ import { useState, useEffect } from 'react';
 
 const useForm = (callback, validate) => {
   const [values, setValues] = useState({
-    username: '',
     email: '',
     password: '',
-    password2: ''
+    password2: '',
+    phoneno: '',
+    dob: ''
   });
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
